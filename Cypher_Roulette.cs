3ef4ffdfe65fa4eb69250562e50e0ac431d91a6c
Nayden Hristov <n_hristov@mail.bg>
2017-03-30 23:47:04 +0300

    Cypher Roulette - Solution

---
+++ Cypher_Roulette.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Task17_Cypher_Roulette
+{
+    class Cypher_Roulette
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string  prevWord = (Console.ReadLine());
+            
+            string firstString = "", secondString = "", result = "";
+            byte counterSpins = 0;
+            int startValue = 0;
+
+            if (prevWord.CompareTo("spin") != 0)
+            {
+                result = prevWord;
+                startValue++;
+            }
+            else
+            {
+                counterSpins++;
+            }
+            
+            for (int i = startValue; i < n; i++)
+            {
+                string currentWord = (Console.ReadLine());
+
+                if (currentWord.CompareTo(prevWord) == 0)
+                {
+                    result = "";
+                    firstString = "";
+                    secondString = "";
+                    counterSpins = 0;
+                    if (currentWord.CompareTo("spin") == 0)
+                    {
+                        i--;
+                        currentWord = "";
+                    }
+                }
+                else if (counterSpins % 2 == 0)
+                {
+                    if (currentWord.CompareTo("spin") == 0)
+                    {
+                        counterSpins++;
+                        secondString = result;
+                        firstString = "";
+                        i--;
+                    }
+                    else
+                    {
+                        firstString = result;
+                        secondString = currentWord;
+                    }
+                }
+                else
+                {
+                    if (currentWord.CompareTo("spin") == 0)
+                    {
+                        counterSpins++;
+                        firstString = result;
+                        secondString = "";
+                        i--;
+                    }
+                    else
+                    {
+                        secondString = result;
+                        firstString = currentWord;
+                    }
+                }
+                                
+                result = firstString + secondString;
+                prevWord = currentWord;
+                }
+            
+            Console.WriteLine(result);
+        } 
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # Cypher_Roulette
-Description - in ReadMe
+You will be given an integer N. On the next N lines, you will receive some strings.
+The strings will be either:
+•	sequences of random characters
+•	or the command - "spin"
+
+If they are normal random characters, you should append them to one another in the cypher string.
+If the command "spin" is entered, every string entered after it should be appended at the start
+of the cypher string, if the command "spin" is entered again after that, you should again begin to append
+them at the end of the cypher string. And so, the append direction changes each time you enter the command "spin".
+If two equal strings are entered two consecutive times, the cypher resets - emptying the cypher string. This rule also applies to the "spin" command.
+Note: the "spin" commands do not count towards the N count.
+Examples:
+
+Input:
+			
+5
+Bed
+Is
+So
+Fine
+spin
+This
+	
+Output: 
+ThisBedIsSoFine
+
+---------------
+Input:
+6
+How
+is
+is
+spin
+How
+spin
+_Are
+_You?
+
+Output:	
+How_Are_You?
+
+---------------
+Input:
+5
+That
+spin
+Spin
+spin
+Six
+Shooter
+Cowboy
+
+Output:	SpinThatSixShooterCowboy
+
+---------------	
+Input:
+6
+Your
+spin
+Do
+spin
+Homework
+Homework
+Beer
+spin
+Drink
+
+Output:
+DrinkBeer
+
+
